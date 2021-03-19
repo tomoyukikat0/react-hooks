b3e943aece45766dbe6490a6b58b4ffffd29b072
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-03-20 03:34:03 +0900

    create new event

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,7 @@ const events = (state = [], action) => {
       const event = { title: action.title, body: action.body }
       const length = state.length
       const id = length === 0 ? 1 : state[length - 1].id + 1
-      return [...state, { id, ...event}
+      return [...state, { id, ...event}]
     case 'DELETE_EVENT':
       return state
     case 'DELETE_ALL_EVENTS':
