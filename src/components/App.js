61bcd130a83d1e15eec590af27a33e1dc9714311
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-03-21 01:53:11 +0900

    show events

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState, useReducer } from 'react'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import Event from './Event'
 import reducer from '../reducers'
 
 const App = () => {
@@ -50,6 +51,9 @@ const App = () => {
             <th></th>
           </tr>
         </thead>
+        <tbody>
+          { state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />))}
+        </tbody>
       </table>
     </div>
   );
