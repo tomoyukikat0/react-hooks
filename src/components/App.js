fac0eaee84ec753d8ee51ef9a7cc70aea4ccccc3
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-03-21 18:34:40 +0900

    setup provider

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, { useReducer } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import EventForm from './EventForm'
-
+import AppContext from '../contexts/AppContext'
 import Events from './Events'
 import reducer from '../reducers'
 
@@ -11,10 +11,12 @@ const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch} />
-      <Events state={state} dispatch={dispatch} />
-    </div>
+    <AppContext.Provider value={'Hello, I am a Provider.'}>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />
+      </div>
+    </AppContext.Provider>
   );
 }
 
