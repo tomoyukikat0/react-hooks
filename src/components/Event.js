aae974a7d42c614a49d844c8ca14690efc220128
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-03-21 21:15:41 +0900

    solve prop drilliing

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { DELETE_EVENT } from '../actions'
+import AppContext from '../contexts/AppContext'
 
-const Event = ({ dispatch, event }) => {
+const Event = ({ event }) => {
+  const { dispatch } = useContext(AppContext)
+  const id = event.id
   const handleClickeDeleteButton = () => {
-      const id = event.id
       const result = window.confirm(`イベント(id=${id})を本当に削除しても良いですか`)
       if (result) dispatch({ type: DELETE_EVENT, id })
     }
 
     return (
       <tr>
-      <td>{event.id}</td>
+      <td>{id}</td>
       <td>{event.title}</td>
       <td>{event.body}</td>
       <td><button type="button" className="btn btn-danger" onClick={handleClickeDeleteButton}>削除</button></td>
