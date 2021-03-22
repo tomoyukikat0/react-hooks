d68d9fad507c9e17f1e376c7e1d94d9892c6ae5c
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-03-23 03:36:40 +0900

    inish

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@ import React, { useReducer } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import EventForm from './EventForm'
-import AppContext from '../contexts/AppContext'
 import Events from './Events'
+import OperationLogs from './OperationLogs'
+import AppContext from '../contexts/AppContext'
 import reducer from '../reducers'
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
       <div className="container-fluid">
         <EventForm />
         <Events />
+        <OperationLogs />
       </div>
     </AppContext.Provider>
   );
